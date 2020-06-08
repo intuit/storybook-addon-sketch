02c806730884f9e1825228e9bba1a7abb68e89e3
Andrew Lisowski <lisowski54@gmail.com>
2020-06-08 14:14:45 -0700

    Update preset.ts

--- src/preset.ts
+++ src/preset.ts
@@ -1,8 +1,4 @@
-export function addons(entry = []) {
-  return [...entry, 'storybook-addon-sketch/register'];
-}
-
-export function webpack(config: { entry: string[] } = { entry: [] }) {
-  config.entry.push(require.resolve('storybook-addon-sketch/entry'));
-  return config;
+export default {
+  addons: ['storybook-addon-sketch/register'],
+  entries: ['storybook-addon-sketch/entry']
 }
